2bf322fc4855598451ab62c142c684d8676067b9
macooo3 <marcoysu765@gmail.com>
2025-02-15 23:43:58 -0500

    push2

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const btnNo = document.querySelector(".no-btn");
 const imgNo = document.getElementById("fadeImage");
 const audioNo = document.getElementById("fadeAudio");
 const btnYes = document.querySelector(".yes-btn");
-const heartPage = document.querySelector(".heart-page");
-const questionPage = document.querySelector('.context')
-const heartImg = document.getElementById('heart-img')
+const questionPage = document.querySelector(".context");
+const heartImg = document.querySelectorAll(".cute-pics");
 const btn = document.getElementById("startStop");
 const music = document.getElementById("music");
-
+const heartPage = document.querySelector(".heart-page");
+const modal = document.querySelectorAll(".modal");
+const overlay = document.querySelector('.overlay')
 
 // No button
 btnNo.addEventListener("click", function () {
@@ -24,23 +25,39 @@ btnNo.addEventListener("click", function () {
   btnNo.classList.add("hidden");
 });
 
-
 // Yes button
 btnYes.addEventListener("click", function (e) {
-  e.preventDefault()
-  questionPage.classList.add('hidden')
+  e.preventDefault();
+  questionPage.classList.add("hidden");
   heartPage.classList.remove("hidden");
-  heartPage.classList.add('visible')
-  music.volume = 0.5
-  music.play()
+  heartPage.classList.add("visible");
+  music.volume = 0.25;
+  // music.play()
 });
 
 // clicked imgs
-heartImg.addEventListener('click', function(e){
-e.preventDefault()
+heartPage.addEventListener("click", function (e) {
+  const picNum = e.target.closest(".cute-pics").dataset.num;
+  console.log(picNum);
 
+  if (!picNum) return;
+
+  
+ modal.forEach((m) => {
+    m.classList.remove("modal-active");
+  });
+  
+  document.querySelector(`.modal-msg--${picNum}`).classList.add("modal-active");
+  overlay.classList.remove('hidden')
+ 
+});
+
+overlay.addEventListener('click', function(){
+  overlay.classList.add('hidden')
+  modal.forEach((m) => {
+    m.classList.remove("modal-active");
+  });
 })
-
 
 // heart rainfall
 let btnStatus = "stopped";
@@ -64,12 +81,10 @@ function startStop() {
     btn.innerHTML = "STOP";
     inter = setInterval(createHeart, 100);
     btnStatus = "started";
-   
   } else {
     btn.innerHTML = "START";
     clearInterval(inter);
     btnStatus = "stopped";
-    
   }
 }
 
